Fixed normalization bug added adversarial trainer

src/timm_trainers/attack/Attacker.py
@@ -1,10 +1,10 @@
 from . import AttackSteps
 import torch.nn as nn
 import torch
-
+from tqdm import tqdm
 
 class Attacker:
-    def __init__(self, model, num_iter=20, epsilon=8/255, attack_step=AttackSteps.LinfStep, lr=0.1, restart=True):
+    def __init__(self, model, num_iter=20, epsilon=8/255, attack_step=AttackSteps.LinfStep, lr=0.1, normalizer=None, restart=True, tqdm=True):
         if not isinstance(model, nn.Module):
             raise("Not valid model")
         self.model = model
@@ -12,7 +12,9 @@ class Attacker:
         self.epsilon = epsilon
         self.attack_step = attack_step
         self.lr = lr
+        self.tqdm = tqdm
         self.restart = restart
+        self.normalizer = normalizer
         self.criterion = nn.CrossEntropyLoss()
         self.device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
         model.to(self.device)
@@ -23,14 +25,19 @@ class Attacker:
         adv_X = attack_step.random_restart(X).to(self.device).requires_grad_(True)
         adv_X = adv_X.to(self.device)
         best_loss = None
-        best_X = None
+        best_X = None 
         iter_no_change = 0
-
+        iterat = range(self.num_iter)
+        if self.tqdm is True:
+            iterat = tqdm(iterat)
         for i in range(self.num_iter):
             if iter_no_change > 10 and self.restart is True:
                 adv_X = attack_step.random_restart(adv_X)
             adv_X = adv_X.detach().clone().requires_grad_(True)
-            output = self.model(adv_X)
+            if self.normalizer is not None:
+                output = self.model(self.normalizer(adv_X))
+            else:
+                output = self.model(adv_X)
             loss = self.criterion(output, y)
             loss.backward()
             if best_loss is None or best_loss < loss.item():

src/timm_trainers/trainers/adv_trainer.py
@@ -1,12 +1,12 @@
 import torch.nn as nn
 import torch
 import timm
-from attack import Attacker, AttackSteps
+from ..attack import Attacker, AttackSteps
 from tqdm import tqdm
 from torchvision import transforms
 from torch.utils import data
 
-class BasicTrainer:
+class AdversarialTrainer:
     def __init__(self, model="resnet18", pretrained=False, criterion=nn.CrossEntropyLoss(), num_classes=10, lr=0.001, epochs=100, adv_step=AttackSteps.L2Step, adv_iter=20, adv_epsilon=0.5, optimizer=torch.optim.Adam, weight_decay=0.0, trainset=None, testset=None, batch_size=20, transforms_train=transforms.Compose([transforms.ToTensor()]), transforms_test=transforms.Compose([transforms.ToTensor()]), input_normalizer=None, resume_path=None, save_path="./"):
         if isinstance(model, str):
             self.model = timm.create_model(model_name=model, pretrained=pretrained, num_classes=num_classes)
@@ -26,7 +26,7 @@ class BasicTrainer:
         self.epochs = epochs
         self.epoch=0
         self.loss = 0
-        self.attacker = Attacker(model=self.model, epsilon=adv_epsilon, attack_step=adv_step, num_iter=adv_iter)
+        self.attacker = Attacker(model=self.model, epsilon=adv_epsilon, attack_step=adv_step, num_iter=adv_iter, tqdm=False)
         if not isinstance(trainset, data.Dataset):
             raise Exception("Not valid train loader")
         if not isinstance(testset, data.Dataset):
@@ -42,6 +42,8 @@ class BasicTrainer:
         if input_normalizer is not None:
             if not isinstance(input_normalizer, transforms.Normalize):
                 raise Exception("Non valid input normalizer")
+            else:
+                self.attacker.normalizer = input_normalizer
         self.normalizer = input_normalizer
         print(f"Model created on device {self.device}")
         if resume_path is not None:
@@ -50,14 +52,14 @@ class BasicTrainer:
             self.optimizer.load_state_dict(checkpoint['optimizer_state_dict'])
             print(f"Model resumed: Epoch {checkpoint['epoch']}")
             self.epoch = checkpoint['epoch']+1
-    def eval(self):
+    def eval_nat(self):
         self.model.eval()
         accuracy = 0.0
         total = 0.0
         running_loss = 0.0
         with tqdm(self.testloader, unit="batch") as tepoch:
             for b, data in enumerate(tepoch):
-                tepoch.set_description(f"Val")
+                tepoch.set_description(f"Nat Val")
                 images, labels = data
                 images = images.to(self.device)
                 if self.normalizer is not None:
@@ -109,7 +111,10 @@ class BasicTrainer:
                         X = self.normalizer(X)
                     y = y.to(self.device)
                     self.optimizer.zero_grad()
-                    outputs = self.model(X)
+                    adv_ex = self.attacker(X, y)
+                    if self.normalizer is not None:
+                        adv_ex = self.normalizer(adv_ex)
+                    outputs = self.model(adv_ex)
                     loss = self.criterion(outputs, y)
                     predictions = outputs.argmax(dim=1, keepdim=True).squeeze()
                     total += y.size(0)
@@ -118,7 +123,8 @@ class BasicTrainer:
                     loss.backward()
                     self.optimizer.step()
                     tepoch.set_postfix(loss=(running_loss/(b+1)), accuracy=(100 * accuracy/total))
-            val_loss = self.eval()
+            self.eval_nat()
+            self.eval_adv()
             torch.save({
                     'epoch': i,
                     'model_state_dict': self.model.state_dict(),