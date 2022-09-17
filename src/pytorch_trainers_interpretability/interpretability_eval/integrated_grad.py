1aa9b6431dc8c2056a65148b48fe0432894438bf
Delyan Boychev <delyan.boychev05@gmail.com>
2022-09-17 19:35:31 +0300

    Fix integrated grad

--- src/pytorch_trainers_interpretability/interpretability_eval/integrated_grad.py
+++ src/pytorch_trainers_interpretability/interpretability_eval/integrated_grad.py
@@ -22,24 +22,31 @@ class IntegratedGrad:
             inp_tensor = self.normalizer(inp_tensor)
         return inp_tensor
     def _pred_grad(self, input, target_label_idx):
-        gradients = []
-        for inp in input:
-            inp = inp.requires_grad_(True)
-            output = self.model(inp)
-            output = F.softmax(output, dim=1)[0, target_label_idx]
-            output.backward()
-            gradient = inp.grad
-            gradients.append(gradient)
-        return torch.cat(gradients)
+        input = input.requires_grad_(True)
+        output = self.model(input)
+        output = F.softmax(output, dim=-1)[0, target_label_idx]
+        output.backward()
+        gradient = input.grad
+        return gradient, output.item()
     def _generate_staturate_batches(self, input, baseline, steps):
-        return [baseline + a * (input - baseline) for a in np.linspace(0, 1, steps)]
-    def _integrated_grads(self, input, target_label_idx, baseline, steps=50):
+        alphas = torch.linspace(0.0, 1.0, steps+1).to(self.device)
+        alphas = alphas[:, None, None, None]
+        images = baseline + alphas*(input-baseline)
+        return images
+    def _integrated_grads(self, input, target_label_idx, baseline, steps=50, batch_size=30):
         scaled_inputs = self._generate_staturate_batches(input, baseline, steps)
-        grads = self._pred_grad(scaled_inputs,target_label_idx)
-        grads = (grads[:-1] + grads[1:]) / 2.0
-        integrated_grads = torch.mean(grads, axis=0)
-        delta_X = (input - baseline).detach().squeeze(0)
-        integrated_grads = (delta_X * integrated_grads).detach().cpu().numpy()
+        gradients = []
+        for i in range(0, steps, batch_size):
+            start = i
+            end = min(start+1, steps)
+            batch = scaled_inputs[start:end]
+            gradient, _ = self._pred_grad(batch, target_label_idx)
+            gradients.append(gradient)
+        gradients = torch.cat(gradients)
+        gradients = (gradients[:-1] + gradients[1:]) / 2.0
+        integrated_grads = torch.mean(gradients, axis=0)
+        delta_X = (input - baseline).to(self.device)
+        integrated_grads = (delta_X*integrated_grads).squeeze(0).detach().cpu().numpy()
         integrated_grads = np.transpose(integrated_grads, (1, 2, 0))
         return integrated_grads
     def _get_attribution_mask(self, integrated_grads):
@@ -54,27 +61,27 @@ class IntegratedGrad:
         mask[mask<treshhold] = 0
         overlay = np.clip(image*0.7 + mask, 0, 1)
         return mask, overlay
-    def zero_baseline_integrated_grads(self, input, target_label_idx, steps=50):
+    def black_baseline_integrated_grads(self, input, target_label_idx, steps=50, batch_size=30):
         input = self._pre_processing(input)
         baseline = torch.zeros(input.shape).to(self.device)
-        return self._integrated_grads(input, target_label_idx, baseline, steps)
-    def gaussian_noise_integrated_grads(self, input, target_label_idx, steps, num_random_trials):
+        return self._integrated_grads(input, target_label_idx, baseline, steps, batch_size)
+    def gaussian_noise_integrated_grads(self, input, target_label_idx, steps, num_random_trials, batch_size=30):
         all_intgrads = []
         input = self._pre_processing(input)
         itr = tqdm(range(num_random_trials), unit="trial")
         for i in itr:
             integrated_grad = self._integrated_grads(input, target_label_idx, \
-                                                    baseline=torch.normal(0, 0.4, input.shape).to(self.device), steps=steps)
+                                                    baseline=torch.normal(0, 0.4, input.shape).to(self.device), steps=steps, batch_size=batch_size)
             all_intgrads.append(integrated_grad)
         avg_intgrads = np.average(np.array(all_intgrads), axis=0)
         return avg_intgrads
-    def random_baseline_integrated_grads(self, input, target_label_idx, steps, num_random_trials):
+    def random_baseline_integrated_grads(self, input, target_label_idx, steps, num_random_trials, batch_size=30):
         all_intgrads = []
         input = self._pre_processing(input)
         itr = tqdm(range(num_random_trials), unit="trial")
         for i in itr:
             integrated_grad = self._integrated_grads(input, target_label_idx, \
-                                                    baseline=torch.rand(input.shape).to(self.device), steps=steps)
+                                                    baseline=torch.rand(input.shape).to(self.device), steps=steps, batch_size=batch_size)
             all_intgrads.append(integrated_grad)
         avg_intgrads = np.average(np.array(all_intgrads), axis=0)
         return avg_intgrads
